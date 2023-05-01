cda4a48452d800dfcc84a22c48223dbd81b84e92
Dmitrii Fedorov <benifed18@gmail.com>
2023-05-01 13:40:52 +0400

    new commit

--- src/main/java/model/Aviary.java
+++ src/main/java/model/Aviary.java
@@ -6,39 +6,31 @@ import java.util.HashMap;
 
 public class Aviary <T extends Animal>{
 
-    private int aviaryMap;
-
     private Size size;
     public Aviary(Size size) {
         this.size = size;
-        this.aviary = new HashMap<String, T>();
-        this.aviaryMap = aviary.size();
+        this.aviaryMap = new HashMap<String, T>();
     }
 
-    private HashMap<String, T> aviary;
+    private HashMap<String, T> aviaryMap;
 
-    public int getAviaryMap() {
-        return aviaryMap;
-    }
+
 
     public void addAnimal(Animal animal) {
         try{
-            aviary.put(animal.getName(), (T) animal);
+            aviaryMap.put(animal.getName(), (T) animal);
         }catch (WrongSizeException e){
             throw e;
-        }finally {
-            this.aviaryMap = aviary.size();
         }
     }
 
     public Animal getAnimal(String name){
-        return (Animal) aviary.get(name);
+        return (Animal) aviaryMap.get(name);
     }
 
     public boolean removeAnimal( String name){
-        if(aviary.containsKey(name)){
-            aviary.remove(name);
-            this.aviaryMap = aviary.size();
+        if(aviaryMap.containsKey(name)){
+            aviaryMap.remove(name);
             return true;
         }else return false;
     }
